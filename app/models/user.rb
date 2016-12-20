42149debf7e4c6a0d4e80b8aa388b91b9cc7ea0b
Nathan Herrera <nathanherrera@gmail.com>
2016-12-19 19:19:05 -0800

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :projects
   has_many :comments
   has_many :posts, through: :projects
+
+  validates_presence_of :username, :email, :password
+  validates_uniqueness_of :username, :email
+
+  validates :password, confirmation: true
 end
